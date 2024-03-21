c2333624b0d8ff9b488d81ed448c130d52ec2056
JCamero <jcamero.urko@gmail.com>
2024-03-21 12:39:20 +0100

    Commit: Resultado final

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <!-- Realizado por Juan Camero Muñoz -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Examen final</title>
+    <title>Prueba Git - AJAX</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
